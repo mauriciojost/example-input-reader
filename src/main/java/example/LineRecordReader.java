907d6b1c8e275bd63ee6beb682e7a4fc1f4999f7
Mauricio Jost <mauricio.jost@gmail.com>
2017-05-15 19:10:44 +0200

    Use original files of 2.2.0 version as starters (Spark 1.6)

--- src/main/java/example/LineRecordReader.java
+++ src/main/java/example/LineRecordReader.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package example;
 
 import java.io.IOException;
@@ -20,6 +38,8 @@ import org.apache.hadoop.io.compress.Decompressor;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.util.LineReader;
 import org.apache.commons.logging.LogFactory;
 import org.apache.commons.logging.Log;
 
@@ -36,7 +56,7 @@ public class LineRecordReader extends RecordReader<LongWritable, Text> {
   private long start;
   private long pos;
   private long end;
-  private SplitLineReader in;
+  private LineReader in;
   private FSDataInputStream fileIn;
   private Seekable filePosition;
   private int maxLineLength;
@@ -68,34 +88,40 @@ public class LineRecordReader extends RecordReader<LongWritable, Text> {
     
     CompressionCodec codec = new CompressionCodecFactory(job).getCodec(file);
     if (null!=codec) {
-      isCompressedInput = true;
+      isCompressedInput = true;	
       decompressor = CodecPool.getDecompressor(codec);
       if (codec instanceof SplittableCompressionCodec) {
         final SplitCompressionInputStream cIn =
           ((SplittableCompressionCodec)codec).createInputStream(
             fileIn, decompressor, start, end,
             SplittableCompressionCodec.READ_MODE.BYBLOCK);
-        in = new CompressedSplitLineReader(cIn, job,
-            this.recordDelimiterBytes);
+        if (null == this.recordDelimiterBytes){
+          in = new LineReader(cIn, job);
+        } else {
+          in = new LineReader(cIn, job, this.recordDelimiterBytes);
+        }
+
         start = cIn.getAdjustedStart();
         end = cIn.getAdjustedEnd();
         filePosition = cIn;
       } else {
-        if (start != 0) {
-          // So we have a split that is only part of a file stored using
-          // a Compression codec that cannot be split.
-          throw new IOException("Cannot seek in " +
-              codec.getClass().getSimpleName() + " compressed stream");
+        if (null == this.recordDelimiterBytes) {
+          in = new LineReader(codec.createInputStream(fileIn, decompressor),
+              job);
+        } else {
+          in = new LineReader(codec.createInputStream(fileIn,
+              decompressor), job, this.recordDelimiterBytes);
         }
-
-        in = new SplitLineReader(codec.createInputStream(fileIn,
-            decompressor), job, this.recordDelimiterBytes);
         filePosition = fileIn;
       }
     } else {
       fileIn.seek(start);
-      in = new UncompressedSplitLineReader(
-          fileIn, job, this.recordDelimiterBytes, split.getLength());
+      if (null == this.recordDelimiterBytes){
+        in = new LineReader(fileIn, job);
+      } else {
+        in = new LineReader(fileIn, job, this.recordDelimiterBytes);
+      }
+
       filePosition = fileIn;
     }
     // If this is not the first split, we always throw away first record
@@ -111,7 +137,7 @@ public class LineRecordReader extends RecordReader<LongWritable, Text> {
   private int maxBytesToConsume(long pos) {
     return isCompressedInput
       ? Integer.MAX_VALUE
-      : (int) Math.max(Math.min(Integer.MAX_VALUE, end - pos), maxLineLength);
+      : (int) Math.min(Integer.MAX_VALUE, end - pos);
   }
 
   private long getFilePosition() throws IOException {
@@ -122,39 +148,6 @@ public class LineRecordReader extends RecordReader<LongWritable, Text> {
       retVal = pos;
     }
     return retVal;
-  }
-
-  private int skipUtfByteOrderMark() throws IOException {
-    // Strip BOM(Byte Order Mark)
-    // Text only support UTF-8, we only need to check UTF-8 BOM
-    // (0xEF,0xBB,0xBF) at the start of the text stream.
-    int newMaxLineLength = (int) Math.min(3L + (long) maxLineLength,
-        Integer.MAX_VALUE);
-    int newSize = in.readLine(value, newMaxLineLength, maxBytesToConsume(pos));
-    // Even we read 3 extra bytes for the first line,
-    // we won't alter existing behavior (no backwards incompat issue).
-    // Because the newSize is less than maxLineLength and
-    // the number of bytes copied to Text is always no more than newSize.
-    // If the return size from readLine is not less than maxLineLength,
-    // we will discard the current line and read the next line.
-    pos += newSize;
-    int textLength = value.getLength();
-    byte[] textBytes = value.getBytes();
-    if ((textLength >= 3) && (textBytes[0] == (byte)0xEF) &&
-        (textBytes[1] == (byte)0xBB) && (textBytes[2] == (byte)0xBF)) {
-      // find UTF-8 BOM, strip it.
-      LOG.info("Found UTF-8 BOM and skipped it");
-      textLength -= 3;
-      newSize -= 3;
-      if (textLength > 0) {
-        // It may work to use the same buffer and not do the copyBytes
-        textBytes = value.copyBytes();
-        value.set(textBytes, 3, textLength);
-      } else {
-        value.clear();
-      }
-    }
-    return newSize;
   }
 
   public boolean nextKeyValue() throws IOException {
@@ -168,15 +161,11 @@ public class LineRecordReader extends RecordReader<LongWritable, Text> {
     int newSize = 0;
     // We always read one extra line, which lies outside the upper
     // split limit i.e. (end - 1)
-    while (getFilePosition() <= end || in.needAdditionalRecordAfterSplit()) {
-      if (pos == 0) {
-        newSize = skipUtfByteOrderMark();
-      } else {
-        newSize = in.readLine(value, maxLineLength, maxBytesToConsume(pos));
-        pos += newSize;
-      }
-
-      if ((newSize == 0) || (newSize < maxLineLength)) {
+    while (getFilePosition() <= end) {
+      newSize = in.readLine(value, maxLineLength,
+          Math.max(maxBytesToConsume(pos), maxLineLength));
+      pos += newSize;
+      if (newSize < maxLineLength) {
         break;
       }
 
@@ -222,7 +211,6 @@ public class LineRecordReader extends RecordReader<LongWritable, Text> {
     } finally {
       if (decompressor != null) {
         CodecPool.returnDecompressor(decompressor);
-        decompressor = null;
       }
     }
   }
